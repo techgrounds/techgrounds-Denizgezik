a09641ed855b32236c9879ec0601a265b24882d6
Denizgezik <134807753+Denizgezik@users.noreply.github.com>
2024-02-07 11:59:41 +0100

    small adjustnents to my management server

--- foundation/fresh/fresh/fresh_stack.py
+++ foundation/fresh/fresh/fresh_stack.py
@@ -1,10 +1,9 @@
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2
-
 )
-
 from constructs import Construct
+from aws_cdk import CfnOutput
 
 class FreshStack(Stack):
 
@@ -12,7 +11,7 @@ class FreshStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         #  Create a custome VPC
-        vpc_1 = ec2.Vpc(self, "Network-1",
+        vpc_A = ec2.Vpc(self, "Network-A",
             ip_addresses=ec2.IpAddresses.cidr("10.10.10.0/24"),
             availability_zones=['eu-central-1a', 'eu-central-1b'],
             subnet_configuration=[
@@ -25,7 +24,7 @@ class FreshStack(Stack):
         )
 
         # Create the management server VPC
-        vpc_2 = ec2.Vpc(self, "Network-2",
+        vpc_B = ec2.Vpc(self, "Network-B",
             ip_addresses=ec2.IpAddresses.cidr("10.20.20.0/24"),
             availability_zones=['eu-central-1a', 'eu-central-1b'],
             subnet_configuration=[
@@ -38,28 +37,29 @@ class FreshStack(Stack):
         )
         # Establish VPC peering connection
         vpc_peering = ec2.CfnVPCPeeringConnection(self, 'VpcPeering',
-            peer_vpc_id=vpc_2.vpc_id,
-            vpc_id=vpc_1.vpc_id
+            peer_vpc_id=vpc_B.vpc_id,
+            vpc_id=vpc_A.vpc_id
         )
         # Modify Route table of the custom VPC private table to add the peering route
         vpc_1_public_RT = ec2.CfnRoute(self, "PublicRT",
-                                        destination_cidr_block= vpc_2.vpc_cidr_block,
-                                        route_table_id= vpc_1.public_subnets[0].route_table.route_table_id,
+                                        destination_cidr_block= vpc_B.vpc_cidr_block,
+                                        route_table_id= vpc_A.public_subnets[0].route_table.route_table_id,
                                         vpc_peering_connection_id=vpc_peering.attr_id
                                         )
         
         # Modify Route table of the default VPC Public table to add the peering route
         vpc_2_private_RT = ec2.CfnRoute(self, "PrivatRT",
-                                        destination_cidr_block=vpc_1.vpc_cidr_block,
-                                        route_table_id= vpc_2.isolated_subnets[0].route_table.route_table_id,
+                                        destination_cidr_block=vpc_A.vpc_cidr_block,
+                                        route_table_id= vpc_B.isolated_subnets[0].route_table.route_table_id,
                                         vpc_peering_connection_id=vpc_peering.attr_id
                                         )
          # Creëer een SG voor de webserver 
         sg_webserver = ec2.SecurityGroup(self,"sgWebServer", 
-                                         vpc = vpc_1,
+                                         vpc = vpc_A,
                                          description = "sg_webserver",
                                          allow_all_outbound = True,
         )
+        
         #open pord webserverSG
         sg_webserver.add_ingress_rule(
             peer=ec2.Peer.ipv4("0.0.0.0/0"),
@@ -89,25 +89,26 @@ class FreshStack(Stack):
 
         # Create webserver:
         self.web_server = ec2.Instance(self, "web_server",
-                                vpc = vpc_1, 
-                                instance_type = ec2.InstanceType("t2.micro"), 
-                                machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023), 
-                                security_group = sg_webserver,
-                                user_data = ec2.UserData.custom(user_data),
-                                vpc_subnets = ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
-                                key_pair= self.key_pair,
-                                block_devices =  [ec2.BlockDevice(
-                                        device_name= "/dev/sdh",
-                                        volume= ec2.BlockDeviceVolume.ebs(8, 
-                                        encrypted = True, 
-                                        delete_on_termination = True
-                                        )                             
-                                    )]
-                                        )
-                                  
+            vpc = vpc_A, 
+            instance_type = ec2.InstanceType("t2.micro"), 
+            machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023), 
+            security_group = sg_webserver,
+            user_data = ec2.UserData.custom(user_data),
+            vpc_subnets = ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            private_ip_address="10.10.10.20",
+            key_pair= self.key_pair,
+            block_devices =  [ec2.BlockDevice(
+                    device_name= "/dev/sdh",
+                    volume= ec2.BlockDeviceVolume.ebs(8, 
+                    encrypted = True, 
+                    delete_on_termination = True)                             
+                )
+            ]
+        )
+              
         # Creëer een SG voor de Management server  
         sg_managserver = ec2.SecurityGroup(self,"sgmanagServer", 
-                                         vpc = vpc_2,
+                                         vpc = vpc_B,
                                          description = "sg_managserver",
                                          allow_all_outbound = True,
         )
@@ -115,11 +116,9 @@ class FreshStack(Stack):
             peer=ec2.Peer.ipv4("0.0.0.0/0"),
             connection=ec2.Port.tcp(22)),
         'allow SSH traffic from anywhere',
-        with open("my_project/user-data-mang.sh", "r") as file:
-            mang_user_data = file.read()
         
         # Create a key pair
-        self.key_pair = ec2.KeyPair(
+        self.key_pair_manag = ec2.KeyPair(
             self, "KeyPairmanag",
             key_pair_name="keypair-name_manag",
             type=ec2.KeyPairType.RSA
@@ -127,17 +126,29 @@ class FreshStack(Stack):
         
         # Create the management server EC2 instance
         self.management_server = ec2.Instance(self, "managServer",
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO)),
-            machine_image=ec2.WindowsImage(ec2.WindowsVersion.WINDOWS_SERVER_2022_ENGLISH_FULL_BASE,
-            user_data=ec2.UserData.custom(mang_user_data),
-            vpc=vpc_2,
+            instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+            machine_image=ec2.WindowsImage(ec2.WindowsVersion.WINDOWS_SERVER_2022_ENGLISH_FULL_BASE),
+            vpc=vpc_B,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            private_ip_address="10.20.20.20",
             key_name="keypair-name_manag",
             security_group=sg_managserver,
-             block_devices=[
+            block_devices=[
                 ec2.BlockDevice(
                     device_name="/dev/sda1",
                     volume=ec2.BlockDeviceVolume.ebs(30, encrypted=True)
                 )
             ],
         )
+
+        # Get the private IPV 4 of the private instance
+        CfnOutput(self,
+                  "myprivateIp",
+                  value=self.web_server.instance_private_ip,
+                  export_name="privateIpv4")
+        
+        # Get the public IPV 4 of the public instance
+        CfnOutput(self,
+                  "public",
+                  value=self.management_server.instance_public_ip,
+                  export_name="PublicIpv4")
