214dca36458f7be2c4eaafe7776464217ad59915
Denizgezik <134807753+Denizgezik@users.noreply.github.com>
2024-02-16 18:31:13 +0100

    changed user data to test autoscaling

--- 10_Final_Project/v1.0/fresh/fresh/user_data.sh
+++ 10_Final_Project/v1.0/fresh/fresh/user_data.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo su 
-echo "<h1>Hello Mother Earth from CDK to Pyhton a.k.a The Finest SlangenBezweerders!</h1>" > /var/www/html/index.html
+# perform a quick software update on instance
+sudo dnf update -y
+# install the latest versions of Apache web server and PHP packages for AL2023
+sudo dnf install -y httpd wget php-fpm php-mysqli php-json php php-devel
+# Download Lab files
+sudo wget https://aws-tc-largeobjects.s3.amazonaws.com/CUR-TF-100-RESTRT-1/80-lab-vpc-web-server/lab-app.zip
+sudo unzip lab-app.zip -d /var/www/html/
+# Turn on web server
+sudo chkconfig httpd on
+sudo service httpd start
+
+
+# #!/bin/bash
+# sudo yum install -y httpd
+# sudo systemctl start httpd
+# sudo systemctl enable httpd
+# sudo su 
+# echo "<h1>Hello Mother Earth from CDK to Pyhton a.k.a The Finest SlangenBezweerders!</h1>" > /var/www/html/index.html
